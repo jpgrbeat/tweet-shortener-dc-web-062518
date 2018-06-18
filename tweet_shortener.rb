b1ea4beb8a80e18d3c3f73fa06d21965545ac3e5
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-18 23:24:39 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,24 +4,24 @@ def dictionary
   dictionary = {:hello => 'hi', :to => '2', :too => '2', :two => '2', :for => '4',:For => '4', :four => '4', :be => 'b', :you => 'u', :at => '@', :and => '&'}
 end
 def word_substituter(tweet)
-  arr = tweet.split(" ")
-  keys = dictionary.keys
-  final = []
-  i = 0
-  j = 0 
-  while i < arr.length 
-    while j < keys.length
-      if arr[i]==keys[j].to_s
-        arr[i] = dictionary[keys[j]]
+  # arr = tweet.split(" ")
+  # keys = dictionary.keys
+  # final = []
+  # i = 0
+  # j = 0 
+  # while i < arr.length 
+  #   while j < keys.length
+  #     if arr[i]==keys[j].to_s
+  #       arr[i] = dictionary[keys[j]]
          
-      end
-      j += 1
-    end
-    j = 0
-    i += 1
-  end
-  arr.join(" ")  
-   #tweet.split.map{|word| dictionary.keys.include?(word) ? word = dictionary[word] : word }.join
+  #     end
+  #     j += 1
+  #   end
+  #   j = 0
+  #   i += 1
+  # end
+  # arr.join(" ")  
+   tweet.split.map{|word| dictionary.keys.include?(word) ? word = dictionary[word] : word }.join(" ")
  end
 
 def bulk_tweet_shortener(tweets)
